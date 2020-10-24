7203a59873cf594ab23b64239e359b0bf7725d71
Sunil Bhatia <sunilbhatia79@gmail.com>
2020-10-24 17:47:33 +0530

    Fixing formatting for prime file

--- numbers/src/numbers/library/primary.clj
+++ numbers/src/numbers/library/primary.clj
@@ -1,19 +1,9 @@
 (ns numbers.library.primary)
 
 (defn prime? [n]
-
   (let [num-range (range 2 (+ 1 (int (Math/sqrt n))))
-        divisible (filter #(= (mod n %) 0) num-range)
-        ]
-
-    (empty? divisible)
-  )
-  )
-  ;(loop [n n]
-  ;  (println n)
-  ;  (if (= n 0)
-  ;    false
-  ;    (recur (dec n)))))
+        divisible (filter #(= (mod n %) 0) num-range)]
+    (empty? divisible)))
 
 (defn is-even? [n] (= (mod n 2) 0))
 
