3a34857858cc31994bdbf4b0ba2a493a1549c869
Sunil Bhatia <sunilbhatia79@gmail.com>
2020-11-19 20:30:22 +0530

    Refactored the code for number guesser to make it functional

--- guessnumber/src/application/core.clj
+++ guessnumber/src/application/core.clj
@@ -7,74 +7,50 @@
   (println "2. End the Game")
   (println "Select: "))
 
-(defn start-game []
+(defn get-number-from-cmd-line []
+  (Integer/parseInt (read-line)))
 
-  (def random-number (rand-int 100))
-  (def flag (atom true))
-  (def input-number (atom 0))
-  (def tries (atom 0))
+(defn evaluate-not-equal [selection entry]
+
+  (cond
+    (< entry selection) (println "Your guess is lower")
+    (> entry selection) (println "Your guess is higher"))
+
+  false)
+
+(defn evaluate [selection entry]
+
+  (if (= selection entry)
+    true
+    (evaluate-not-equal selection entry)))
+
+(defn start-game [random-number]
 
   (println "I have a number between 1 and 100, can you guess it in 10 tries?")
 
-  (while @flag
-    (do
-
-      (when (= @tries 10)
-        (println (str "You failed to guess in 10 tries. I had thought of the number " random-number))
-        (reset! flag false))
-
-      (swap! tries inc)
-
-      ;(println random-number)
-
-      (reset! input-number (->> (read-line) (Integer/parseInt)))
-
-      (if (= @input-number random-number)
-
-        (do
-          (println (str "You guessed it right in " @tries " tries"))
-          (reset! flag false))
-
-        (do
-          (if (< @input-number random-number)
-            (println "Your guess is lower")
-            (println "Your guess is higher")))))))
+  (loop [tries 0 input-number (get-number-from-cmd-line)]
+    (if (evaluate random-number input-number)
+      (do (println (str "You guessed it right in " tries " tries")))
+      (recur (inc tries) (get-number-from-cmd-line)))))
 
 (defn end-game [] (println "Thank you for playing the guessing game. Please play again later."))
 (defn print-usage [] (println "Please choose either option 1 or option 2"))
 
 (defn command-loop []
-  (loop [choice (read-line)]
-
-    (def choice-number (Integer/parseInt choice))
+  (loop [choice (get-number-from-cmd-line)]
 
     (cond
-      (= choice-number 1) (start-game)
-      (= choice-number 2) (end-game)
+      (= choice 1) (start-game (rand-int 100))
+      (= choice 2) (end-game)
       :else (print-usage))
 
-    (if (= choice-number 2)
+    (if (= choice 2)
       nil
       (do
         (print-menu)
-        (recur (read-line))))))
+        (recur (get-number-from-cmd-line))))))
 
 (defn start-application []
-
   (println "Welcome to the Guessing Number Game...")
   (print-menu)
   (command-loop))
-
-(defn test-sunil []
-
-  (let [c (->> (read-line)
-               (Integer/parseInt))]
-    ;(println (+ c 100))
-
-    (cond
-      (= c 1) (println "One")
-      (= c 2) (println "Two")
-      :else "W  rong Option")))
-
-
-
